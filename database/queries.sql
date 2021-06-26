03891696970f0bb4e20779e79df6abaa498a0f9b
Jonas Paulikas <70077913+paulikas78@users.noreply.github.com>
2021-06-26 14:46:44 -0500

    backend finished for 3rd assessment

--- database/queries.sql
+++ database/queries.sql
@@ -6,17 +6,31 @@ WHERE c.e_mail ILIKE '%gmail%';
 
 
   --2. Retrieve the address of the customers and the order IDs for all orders that were placed in 2020
-SELECT *
-FROM addresses AND id FROM orders
-WHERE order_date ILIKE '2020%';
+SELECT a.street, a.street2, a.city, a.state_or_province, a.zip_code, a.country 
+FROM addresses a 
+JOIN customers 
+ON a.id = customers.address_id 
+JOIN orders
+ON customers.id = orders.customer_id
+WHERE order_date < '2021-01-01' AND order_date  > '2019-12-31';
 
 
   --3. Retrieve all product details for products that are under the "Kitchen" category
 SELECT *
 FROM products
+JOIN product_categories
+ON products.id = product_categories.product_id
 JOIN categories
-ON products.category_id = category.id 
-AND category.category_name = 'Kitchen'; 
+ON product_categories.category_id = categories.id 
+WHERE category_name = 'Kitchen';
 
 
   --4. Retrieve the product names and prices of all products ordered by the customer with first name "Bugs" and last name "Bunny"
+
+SELECT p.prod_name, p.prod_price
+FROM products p
+JOIN orders o
+ON o.product_id = p.id
+JOIN customers c
+ON o.customer_id = c.id
+WHERE first_name = 'Bugs' AND last_name = 'Bunny';
