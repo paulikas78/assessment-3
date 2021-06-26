340df0922ff40822d1ba17b64bcaefad17007b31
Jonas Paulikas <70077913+paulikas78@users.noreply.github.com>
2021-06-26 12:48:00 -0500

    2nd commit

--- database/schema.sql
+++ database/schema.sql
@@ -1,12 +1,8 @@
 -- Schema creation goes here
-DROP TABLE IF EXISTS customers CASCADE;
-create table customers (
+DROP TABLE IF EXISTS categories CASCADE;
+create table categories (
   id                SERIAL PRIMARY KEY,
-  first_name        VARCHAR(255) NOT NULL,
-  last_name         VARCHAR(255) NOT NULL,
-  contact_phone     VARCHAR(30) NOT NULL,
-  address_id        INTEGER, -- FK to addresses.id
-  e_mail            VARCHAR(255) NOT NULL
+  category_name     VARCHAR(255) NOT NULL
 );
 
 DROP TABLE IF EXISTS addresses CASCADE;
@@ -20,6 +16,34 @@ create table addresses (
   country           VARCHAR(255) NOT NULL
 );
 
+DROP TABLE IF EXISTS products CASCADE;
+create table products (
+  id                SERIAL PRIMARY KEY,
+  prod_name         VARCHAR(255) NOT NULL,
+  prod_description  VARCHAR(255) NOT NULL,
+  prod_price        DECIMAL
+);
+
+
+DROP TABLE IF EXISTS customers CASCADE;
+create table customers (
+  id                SERIAL PRIMARY KEY,
+  first_name        VARCHAR(255) NOT NULL,
+  last_name         VARCHAR(255) NOT NULL,
+  contact_phone     VARCHAR(30) NOT NULL,
+  address_id        INTEGER, -- FK to addresses.id
+  e_mail            VARCHAR(255) NOT NULL
+);
+
+
+DROP TABLE IF EXISTS product_categories CASCADE; -- join table for products & categories
+create table product_categories (
+  id                SERIAL PRIMARY KEY,
+  product_id        INTEGER, -- FK reference to products.id
+  category_id       INTEGER -- FK reference to categories.id
+);
+
+
 DROP TABLE IF EXISTS orders CASCADE;
 create table orders (
   id                SERIAL PRIMARY KEY,
@@ -30,37 +54,30 @@ create table orders (
 );
 
 
-DROP TABLE IF EXISTS products CASCADE;
-create table products (
-  id                SERIAL PRIMARY KEY,
-  category_id       INTEGER, -- FK reference to categories.id
-  prod_name         VARCHAR(255) NOT NULL,
-  prod_description  VARCHAR(255) NOT NULL,
-  prod_price        DECIMAL
-);
-
-DROP TABLE IF EXISTS categories CASCADE;
-create table categories (
-  id                SERIAL PRIMARY KEY,
-  category_name     VARCHAR(255) NOT NULL
-);
 
 ALTER TABLE customers
-  ADD CONSTRAINT fk_address_id
-  FOREIGN KEY (address_id)
-  REFERENCES addresses (id);
+    ADD CONSTRAINT fk_address_id 
+    FOREIGN KEY (address_id) 
+    REFERENCES addresses;
+
+ALTER TABLE product_categories
+  ADD CONSTRAINT fk_product_id
+  FOREIGN KEY (product_id)
+  REFERENCES products;
+
+ALTER TABLE product_categories
+  ADD CONSTRAINT fk_category_id
+  FOREIGN KEY (category_id)
+  REFERENCES categories;
 
 ALTER TABLE orders
   ADD CONSTRAINT fk_customer_id
   FOREIGN KEY (customer_id)
-  REFERENCES customers (id);
+  REFERENCES customers;
 
 ALTER TABLE orders
   ADD CONSTRAINT fk_product_id
   FOREIGN KEY (product_id)
-  REFERENCES products (id);
+  REFERENCES products;
 
-ALTER TABLE products
-  ADD CONSTRAINT fk_category_id
-  FOREIGN KEY (category_id)
-  REFERENCES categories (id);
+
--- database/seeds.sql
+++ database/seeds.sql
@@ -1,18 +1,18 @@
--- customers seeds
-INSERT INTO customers (id, first_name, last_name, contact_phone, address_id, e_mail) 
-VALUES (1, 'Henry', 'Rollins', '+1 (312) 545 - 2209', 1,'rollins@blackflag.com');
+-- categories seeds
+INSERT INTO categories (id, category_name)
+VALUES (1, 'Living Room');
 
-INSERT INTO customers (id, first_name, last_name, contact_phone, address_id, e_mail) 
-VALUES (2, 'Bugs', 'Bunny', '+1 (323) 658 - 3141', 2,'bugs@bunny.com');
+INSERT INTO categories (id, category_name)
+VALUES (2, 'Kitchen');
 
-INSERT INTO customers (id, first_name, last_name, contact_phone, address_id, e_mail) 
-VALUES (3, 'Dark', 'Helmet', '+1 (480) 434 - 2415', 3,'helmet@spaceballs.com');
+INSERT INTO categories (id, category_name)
+VALUES (3, 'Home Office');
 
-INSERT INTO customers (id, first_name, last_name, contact_phone, address_id, e_mail) 
-VALUES (4, 'John', 'Riggs', '+1 (416) 946 - 2328', 4,'linux4life@gmail.com');
+INSERT INTO categories (id, category_name)
+VALUES (4, 'Bath Room');
 
-INSERT INTO customers (id, first_name, last_name, contact_phone, address_id, e_mail) 
-VALUES (5, 'Lula', 'da Silva', '+55 41 3240 - 5000', 5,'paz_e_amor@gmail.com');
+INSERT INTO categories (id, category_name)
+VALUES (5, 'Entryway');
 
 
 -- adresses seeds
@@ -32,6 +32,57 @@ INSERT INTO addresses (id, street, street2, city, state_or_province, zip_code, c
 VALUES (5, '1 Rua Isidio Alves Ribeiro', 'Vila Militar', 'Piraquaria', 'PR', '83304-427', 'BRAZIL');
 
 
+-- products seeds
+INSERT INTO products (id, prod_name, prod_description, prod_price)
+VALUES (1, 'Candlestick', 'silver 8 in. / 20 cm. tall Candlestick', 9.99);
+
+INSERT INTO products (id, prod_name, prod_description, prod_price)
+VALUES (2, 'Plate Set', '4 Large, Ceramic Plates', 14.99);
+
+INSERT INTO products (id, prod_name, prod_description, prod_price)
+VALUES (3, 'Laptop Riser', 'Wooden, 2 in. / 5 cm. Riser For All Laptops', 11.99);
+
+INSERT INTO products (id, prod_name, prod_description, prod_price)
+VALUES (4, 'Shaving Mirror', '8 in. / 20 cm. x 4 in. / 10 cm. Mirror w/ Suction Cups / Shower Wall Mounts', 24.99);
+
+INSERT INTO products (id, prod_name, prod_description, prod_price)
+VALUES (5, 'Welcome Mat', '20 in. / 51 cm. x 50 in. / 115 cm. Black Carpeted Mat', 19.99);
+
+
+-- customers seeds
+INSERT INTO customers (id, first_name, last_name, contact_phone, address_id, e_mail) 
+VALUES (1, 'Henry', 'Rollins', '+1 (312) 545 - 2209', 1,'rollins@blackflag.com');
+
+INSERT INTO customers (id, first_name, last_name, contact_phone, address_id, e_mail) 
+VALUES (2, 'Bugs', 'Bunny', '+1 (323) 658 - 3141', 2,'bugs@bunny.com');
+
+INSERT INTO customers (id, first_name, last_name, contact_phone, address_id, e_mail) 
+VALUES (3, 'Dark', 'Helmet', '+1 (480) 434 - 2415', 3,'helmet@spaceballs.com');
+
+INSERT INTO customers (id, first_name, last_name, contact_phone, address_id, e_mail) 
+VALUES (4, 'John', 'Riggs', '+1 (416) 946 - 2328', 4,'linux4life@gmail.com');
+
+INSERT INTO customers (id, first_name, last_name, contact_phone, address_id, e_mail) 
+VALUES (5, 'Lula', 'da Silva', '+55 41 3240 - 5000', 5,'paz_e_amor@gmail.com');
+
+
+-- product categories seeds
+INSERT INTO product_categories (id, product_id, category_id)
+VALUES (1, 1, 1);
+
+INSERT INTO product_categories (id, product_id, category_id)
+VALUES (2, 2, 2);
+
+INSERT INTO product_categories (id, product_id, category_id)
+VALUES (3, 3, 3);
+
+INSERT INTO product_categories (id, product_id, category_id)
+VALUES (4, 4, 4);
+
+INSERT INTO product_categories (id, product_id, category_id)
+VALUES (5, 5, 5);
+
+
 -- orders seeds
 INSERT INTO orders (id, product_id, customer_id, order_date, delivery_date)
 VALUES (1, 1, 1, '2021-06-25', '2021-06-30');
@@ -49,46 +100,16 @@ INSERT INTO orders (id, product_id, customer_id, order_date, delivery_date)
 VALUES (5, 5, 5, '2019-05-20', '2019-05-24');
 
 
--- products seeds
-INSERT INTO products (id, category_id, prod_name, prod_description, prod_price)
-VALUES (1, 1, 'Candlestick', 'silver 8 in. / 20 cm. tall Candlestick', 9.99);
-
-INSERT INTO products (id, category_id, prod_name, prod_description, prod_price)
-VALUES (2, 2, 'Plate Set', '4 Large, Ceramic Plates', 14.99);
-
-INSERT INTO products (id, category_id, prod_name, prod_description, prod_price)
-VALUES (3, 3, 'Laptop Riser', 'Wooden, 2 in. / 5 cm. Riser For All Laptops', 11.99);
-
-INSERT INTO products (id, category_id, prod_name, prod_description, prod_price)
-VALUES (4, 4, 'Shaving Mirror', '8 in. / 20 cm. x 4 in. / 10 cm. Mirror w/ Suction Cups / Shower Wall Mounts', 24.99);
-
-INSERT INTO products (id, category_id, prod_name, prod_description, prod_price)
-VALUES (5, 5, 'Welcome Mat', '20 in. / 51 cm. x 50 in. / 115 cm. Black Carpeted Mat', 19.99);
-
-
--- categories seeds
-INSERT INTO categories (id, category_name)
-VALUES (1, 'Living Room');
-
-INSERT INTO categories (id, category_name)
-VALUES (2, 'Kitchen');
-
-INSERT INTO categories (id, category_name)
-VALUES (3, 'Home Office');
-
-INSERT INTO categories (id, category_name)
-VALUES (4, 'Bath Room');
-
-INSERT INTO categories (id, category_name)
-VALUES (5, 'Entryway');
-
-
 -- set val statements
+SELECT setval('categories_id_seq', (SELECT MAX(id) FROM categories));
 SELECT setval('customers_id_seq', (SELECT MAX(id) FROM customers));
 SELECT setval('addresses_id_seq', (SELECT MAX(id) FROM addresses));
-SELECT setval('orders_id_seq', (SELECT MAX(id) FROM orders));
+SELECT setval('product_categories_id_seq', (SELECT MAX(id) FROM product_categories));
 SELECT setval('products_id_seq', (SELECT MAX(id) FROM products));
-SELECT setval('categories_id_seq', (SELECT MAX(id) FROM categories));
+SELECT setval('orders_id_seq', (SELECT MAX(id) FROM orders));
+
+
+
 
 
 
